165319dd2f52ef423d104b6c0a1c0f0bc5f714e7
Ana <ana.clara.correa.da.silva@ibm.com>
2020-06-01 15:59:23 -0300

    Total e subtotal do Pedido

--- src/main/java/com/anaclara/shopy/domain/ItemPedido.java
+++ src/main/java/com/anaclara/shopy/domain/ItemPedido.java
@@ -30,6 +30,13 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
+
 
 	@JsonIgnore
 	public Pedido getPedido() {
--- src/main/java/com/anaclara/shopy/domain/Pedido.java
+++ src/main/java/com/anaclara/shopy/domain/Pedido.java
@@ -53,6 +53,15 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 
 	public Integer getId() {
 		return id;
