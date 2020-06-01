7f122ddb7159c52bd40b5d393a5ce7e65693a6e9
Ana <ana.clara.correa.da.silva@ibm.com>
2020-06-01 16:06:10 -0300

    Insere Pedido

--- src/main/java/com/anaclara/shopy/domain/ItemPedido.java
+++ src/main/java/com/anaclara/shopy/domain/ItemPedido.java
@@ -42,10 +42,19 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 
 	public ItemPedidoPK getId() {
 		return id;
--- src/main/java/com/anaclara/shopy/services/PedidoService.java
+++ src/main/java/com/anaclara/shopy/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.anaclara.shopy.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.anaclara.shopy.domain.ItemPedido;
+import com.anaclara.shopy.domain.PagamentoComBoleto;
 import com.anaclara.shopy.domain.Pedido;
+import com.anaclara.shopy.domain.enums.EstadoPagamento;
+import com.anaclara.shopy.repository.ItemPedidoRepository;
+import com.anaclara.shopy.repository.PagamentoRepository;
 import com.anaclara.shopy.repository.PedidoRepository;
 import com.anaclara.shopy.services.exceptions.ObjectNotFoundException;
 
@@ -14,11 +20,44 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
