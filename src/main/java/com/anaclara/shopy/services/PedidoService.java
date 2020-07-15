8c591fa384588478e5e2554d04955c6818877371
Ana <ana.clara.correa.da.silva@ibm.com>
2020-07-14 22:06:49 -0300

    Implementa restrição de conteúdo: cliente só recupera pedidos próprios

--- src/main/java/com/anaclara/shopy/services/PedidoService.java
+++ src/main/java/com/anaclara/shopy/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.anaclara.shopy.domain.Cliente;
 import com.anaclara.shopy.domain.ItemPedido;
 import com.anaclara.shopy.domain.PagamentoComBoleto;
 import com.anaclara.shopy.domain.Pedido;
@@ -13,6 +17,8 @@ import com.anaclara.shopy.domain.enums.EstadoPagamento;
 import com.anaclara.shopy.repository.ItemPedidoRepository;
 import com.anaclara.shopy.repository.PagamentoRepository;
 import com.anaclara.shopy.repository.PedidoRepository;
+import com.anaclara.shopy.security.UserSS;
+import com.anaclara.shopy.services.exception.AuthorizationException;
 import com.anaclara.shopy.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,6 +74,17 @@ public class PedidoService {
 		System.out.println(obj);
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
