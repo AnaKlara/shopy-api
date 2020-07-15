a2e7643f9a2484851f1a71b42cb229197363687d
Ana <ana.clara.correa.da.silva@ibm.com>
2020-07-14 22:11:25 -0300

    Cria Refresh Token Resource

---
+++ src/main/java/com/anaclara/shopy/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.anaclara.shopy.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anaclara.shopy.security.JWTUtil;
+import com.anaclara.shopy.security.UserSS;
+import com.anaclara.shopy.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
