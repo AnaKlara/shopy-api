2f030d7eabfac6107d9421f3d62cea5f84ade8d1
Ana <ana.clara.correa.da.silva@ibm.com>
2020-07-23 14:19:15 -0300

    Implementa a geração automática de documentação com swagger

--- src/main/java/com/anaclara/shopy/config/SecurityConfig.java
+++ src/main/java/com/anaclara/shopy/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -52,6 +53,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/clientes/**",
 			"/auth/forgot/**"
 	};
+	
+	
+	// permitir os endpoints do swagger
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(
+				"/v2/api-docs", 
+				"/configuration/ui", 
+				"/swagger-resources/**", 
+				"/configuration/**", 
+				"/swagger-ui.html", 
+				"/webjars/**");
+		}
+	
+	
 
 
 	@Override
