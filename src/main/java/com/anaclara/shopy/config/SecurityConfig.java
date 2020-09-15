e849a2e0c5cb07f431b3a02eca7c291e1a6371f8
Ana <ana.clara.correa.da.silva@ibm.com>
2020-09-14 23:52:55 -0300

    Configurações de Segurança

--- src/main/java/com/anaclara/shopy/config/SecurityConfig.java
+++ src/main/java/com/anaclara/shopy/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
+			"/categorias**",
 			"/estados/**"
 	};
 	
